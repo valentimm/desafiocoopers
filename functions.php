cbbb4eec699fa3a5996f77fc3289915da31f38ba
Matheus Valentim <meduardo.valentim@gmail.com>
2025-08-08 10:01:16 -0300

    feat: add fonts

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * DesafioCoopers functions and definitions
+ * desafio functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package DesafioCoopers
+ * @package desafio
  */
 
 if ( ! defined( '_S_VERSION' ) ) {
@@ -19,14 +19,14 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function desafiocoopers_setup() {
+function desafio_setup() {
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on DesafioCoopers, use a find and replace
-		* to change 'desafiocoopers' to the name of your theme in all the template files.
+		* If you're building a theme based on desafio, use a find and replace
+		* to change 'desafio' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'desafiocoopers', get_template_directory() . '/languages' );
+	load_theme_textdomain( 'desafio', get_template_directory() . '/languages' );
 
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
@@ -49,7 +49,7 @@ function desafiocoopers_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'desafiocoopers' ),
+			'menu-1' => esc_html__( 'Primary', 'desafio' ),
 		)
 	);
 
@@ -74,7 +74,7 @@ function desafiocoopers_setup() {
 	add_theme_support(
 		'custom-background',
 		apply_filters(
-			'desafiocoopers_custom_background_args',
+			'desafio_custom_background_args',
 			array(
 				'default-color' => 'ffffff',
 				'default-image' => '',
@@ -100,7 +100,7 @@ function desafiocoopers_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'desafiocoopers_setup' );
+add_action( 'after_setup_theme', 'desafio_setup' );
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,22 +109,22 @@ add_action( 'after_setup_theme', 'desafiocoopers_setup' );
  *
  * @global int $content_width
  */
-function desafiocoopers_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'desafiocoopers_content_width', 640 );
+function desafio_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'desafio_content_width', 640 );
 }
-add_action( 'after_setup_theme', 'desafiocoopers_content_width', 0 );
+add_action( 'after_setup_theme', 'desafio_content_width', 0 );
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function desafiocoopers_widgets_init() {
+function desafio_widgets_init() {
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'desafiocoopers' ),
+			'name'          => esc_html__( 'Sidebar', 'desafio' ),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'desafiocoopers' ),
+			'description'   => esc_html__( 'Add widgets here.', 'desafio' ),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -132,22 +132,27 @@ function desafiocoopers_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'desafiocoopers_widgets_init' );
+add_action( 'widgets_init', 'desafio_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
  */
-function desafiocoopers_scripts() {
-	wp_enqueue_style( 'desafiocoopers-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'desafiocoopers-style', 'rtl', 'replace' );
+function desafio_scripts() {
+	wp_enqueue_style( 'desafio-style', get_stylesheet_uri(), array(), time() );
 
-	wp_enqueue_script( 'desafiocoopers-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	$font_url = 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Poppins:wght@400;500;600;700&display=swap';
+
+	wp_enqueue_style( 'desafio-fonts', $font_url, array(), null );
+
+	wp_style_add_data( 'desafio-style', 'rtl', 'replace' );
+
+	wp_enqueue_script( 'desafio-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'desafiocoopers_scripts' );
+add_action( 'wp_enqueue_scripts', 'desafio_scripts' );
 
 /**
  * Implement the Custom Header feature.
